e5d161397013d1742907cb5df733d15f73fdfbaa
vinimartinson <vi_hmartinson@hotmail.com>
2016-11-19 02:26:17 -0500

    remov pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
+      puts "=========================== #{user.name}"
     end
   end
 end
